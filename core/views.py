c9a9cd74c2c1078ef7ae28f269928b0c93ff57f3
Joao-Bornelli-Pessoal <joaoluisbornelli@gmail.com>
2025-12-02 22:17:01 -0300

    added inputs for the test header on homescreen and created validations before sending to the function that generates the docx document

--- core/views.py
+++ core/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import login, authenticate, logout
 from .forms import SignupForm
 import json
 
+from core.docxGeneration import generate_docx
+
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -41,43 +43,85 @@ def signup_view(request):
 @login_required
 def home_view(request):
     fields = {'Contexto','Comando','Alternativas','Justificativa do Gabarito','Justificativa dos Distratores','Informações Essenciais'}
-    
+    error = []
     if request.method == 'POST':
+        # Test Header Data Validation
         receivedFile = request.FILES.get('file')
-        if not receivedFile:
-            return render(request, 'home.html', {'error': 'Nenhum arquivo selecionado.'})
+        headerData = {
+            'Course_Unit': request.POST.get('Course_Unit',''),
+            'Instructor': request.POST.get('Instructor',''),
+            'Course': request.POST.get('Course',''),
+        }
         
+        if not receivedFile:
+            error.append('Nenhum arquivo selecionado.')
+            #return render(request, 'home.html', {'error': 'Nenhum arquivo selecionado.'})
+        
+        if headerData['Course_Unit'].strip() == '':
+            error.append('O campo "Unidade Curricular" é obrigatório.')
+            #return render(request, 'home.html', {'course_unit_error': 'O campo "Unidade Curricular" é obrigatório.'})
+        if headerData['Instructor'].strip() == '':
+            error.append('O campo "Docente" é obrigatório.')
+            #return render(request, 'home.html', {'instructor_error': 'O campo "Docente" é obrigatório.'})
+        if headerData['Course'].strip() == '':
+            error.append('O campo "Curso" é obrigatório.')
+            #return render(request, 'home.html', {'course': 'O campo "Curso" é obrigatório.'})
+        
+        if error:        
+            return render(request, 'home.html', {'error': error})
+        
+        
+        
+        # Test File Data Validation
         try:
             jsonData = json.load(receivedFile)
-        except Exception:
-            return render(request, 'home.html', {'error': 'Arquivo inválido.'})
-        
-        if isinstance(jsonData, list):
-            if len(jsonData) == 0:
-                return render(request, 'home.html', {'error': 'Arquivo JSON está vazio.'})
-            else:
-                jsonData = jsonData[0]
+            if isinstance(jsonData, list):
+                if len(jsonData) == 0:
+                    error.append('Arquivo JSON está vazio.')
+                    #return render(request, 'home.html', {'error': 'Arquivo JSON está vazio.'})
+                else:
+                    jsonData = jsonData[0]
+                
+            missing = fields - jsonData.keys() 
+            for field in missing:
+                error.append(f'Campo obrigatório "{field}" ausente no arquivo.')
+                #return render(request, 'home.html', {'error': f'Campo obrigatório "{field}" ausente no arquivo.'})
             
-        missing = fields - jsonData.keys() 
-        for field in missing:
-            return render(request, 'home.html', {'error': f'Campo obrigatório "{field}" ausente no arquivo.'})
+            if len(jsonData['Alternativas'])<3:
+                error.append('O campo "Alternativas" deve conter pelo menos 3 itens.')
+                #return render(request, 'home.html', {'error': 'O campo "Alternativas" deve conter pelo menos 3 itens.'})
+            
+            if len(jsonData['Alternativas'])>4:
+                error.append('O campo "Alternativas" deve conter no máximo 4 itens.')
+                #return render(request, 'home.html', {'error': 'O campo "Alternativas" deve conter no máximo 4 itens.'})
+            
+            for alt in jsonData['Alternativas']:
+                if alt['texto'].strip() == '':
+                    error.append('As alternativas não podem estar vazias.')
+                    #return render(request, 'home.html', {'error': 'As alternativas não podem estar vazias.'})
+                if alt['gabarito'] not in [True,False]:
+                    error.append('O campo "gabarito" das alternativas deve ser True ou False.')
+                    #return render(request, 'home.html', {'error': 'O campo "gabarito" das alternativas deve ser True ou False.'})
+            
+            if sum(1 for alt in jsonData['Alternativas'] if alt.get('gabarito')==True) != 1:
+                error.append('Deve haver ao menos uma alternativa marcada como gabarito (True).')
+                #return render(request, 'home.html', {'error': 'Deve haver ao menos uma alternativa marcada como gabarito (True).'})
+        except Exception:
+            error.append('Arquivo inválido.')
+            #return render(request, 'home.html', {'error': 'Arquivo inválido.'})
         
-        if len(jsonData['Alternativas'])<3:
-            return render(request, 'home.html', {'error': 'O campo "Alternativas" deve conter pelo menos 3 itens.'})
         
-        if len(jsonData['Alternativas'])>4:
-            return render(request, 'home.html', {'error': 'O campo "Alternativas" deve conter no máximo 4 itens.'})
+       
         
-        for alt in jsonData['Alternativas']:
-            if alt['texto'].strip() == '':
-                return render(request, 'home.html', {'error': 'As alternativas não podem estar vazias.'})
-            if alt['gabarito'] not in [True,False]:
-                return render(request, 'home.html', {'error': 'O campo "gabarito" das alternativas deve ser True ou False.'})
+        print('>>headerData',headerData)
+        #generate_docx(headerData, jsonData)
+        if error:
+            return render(request, 'home.html', {'error': error})
         
-        if sum(1 for alt in jsonData['Alternativas'] if alt.get('gabarito')==True) != 1:
-            return render(request, 'home.html', {'error': 'Deve haver ao menos uma alternativa marcada como gabarito (True).'})
         #teste     
         return render(request, 'home.html', {'success': 'Arquivo processado com sucesso!', 'data': jsonData})
+        
+    
         
     
     return render(request, 'home.html')
@@ -92,5 +136,7 @@ def logout_view(request):
         logout(request)
         return redirect('login')
     return redirect('home')
+
+
 
 
